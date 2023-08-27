0bd099635ac932c090f0b52410071869fb6a8f47
Alessandro Pellegrino <alessandro.pellegrino9494@gmail.com>
2023-08-27 13:59:05 +0200

    .

--- food_backend/settings.py
+++ food_backend/settings.py
@@ -170,4 +170,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
