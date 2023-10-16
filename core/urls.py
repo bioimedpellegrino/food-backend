e491ff31883c68c39fa50315f62f1e0304574a13
Alessandro Pellegrino <alessandro.pellegrino9494@gmail.com>
2023-10-16 17:20:39 +0200

    .

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path('food/diet/', DailyFoodView.as_view(), name='food_list'),
     path('food/diet/list/', DailyFoodListView.as_view(), name='food_list'),
-    path('food/<str:food_id>/', FoodDetailsView.as_view(), name='food_details'),
+    path('food/detail/<str:food_id>/', FoodDetailsView.as_view(), name='food_details'),
     path('food/diet/meal_micronutrients/<int:id>/', MealMicronutrientsView.as_view(), name='meal_micro'),
     path('food/log_weight/', WeightMeasureView.as_view(), name='log_weight'),
     path('advices/', AdvicesListView.as_view(), name='advices_list')
